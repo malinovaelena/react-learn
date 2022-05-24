c70897f1e7e98e0b04b56a540a7864543e408be5
Elena Pimenova <elena_pimenova@epam.com>
2022-05-25 02:23:53 +0400

    'creating/components'

---
+++ src/components/FuncComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// function component
+
+const funcComponent = () => {
+  const greeting = 'Hello word, Function Component!';
+
+  return <div>{greeting}</div>;
+};
+
+
+export default funcComponent;
---
+++ src/components/Item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Item = React.createElement("div",
+{}, 'component creating by method "createElement"'
+);
+
+export default Item;
